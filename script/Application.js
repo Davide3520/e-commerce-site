c98cffe7820040ac94daa66705c8e6b16d643e0f
Davide Lepri <davidelepri3@gmail.com>
2021-12-13 15:52:42 -0500

    seed file done

---
+++ script/Application.js
@@ -0,0 +1,23 @@
+
+const jobApp = [
+  {
+  companyName: "Google",
+  companyUrl: "https://www.google.com/",
+  positionTitle: "Junior Software Engineer",
+  response: false
+  },
+  {
+  companyName: "Facebook",
+  companyUrl: "https://www.facebook.com/",
+  positionTitle: "Senior Software Engineer",
+  response: true
+  },
+  {
+  companyName: "IBM",
+  companyUrl: "https://www.ibm.com/",
+  positionTitle: "Junior Software Engineer",
+  response: true
+  }
+]
+
+module.exports = jobApp;
